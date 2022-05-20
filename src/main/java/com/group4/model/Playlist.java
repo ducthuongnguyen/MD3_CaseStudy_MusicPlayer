6346ee54d3da12244b8aca7dc40f26c786c43e29
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-05-20 10:04:20 +0700

    thuong day

--- src/main/java/com/group4/model/Playlist.java
+++ src/main/java/com/group4/model/Playlist.java
@@ -35,7 +35,6 @@ public class Playlist {
     public String getNamePlaylist() {
         return namePlaylist;
     }
-
     public void setNamePlaylist(String namePlaylist) {
         this.namePlaylist = namePlaylist;
     }
